74c1a4fbc465a1429b84c5ce904d85120b2b7383
AbdulRehman <153705975+AbdulRehman817@users.noreply.github.com>
2024-03-07 00:24:10 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,116 @@
+/*
+var sub1=prompt('Enter subject name')
+document.write(sub1+'\n')
+var ObtainedMarks1=+prompt('Enter obtained marks')
+document.write(ObtainedMarks1 +'<br>'+'\n')
+var totalmarks1=100
+
+var sub1=prompt('Enter subject name')
+document.write(sub1 +'\n')
+var ObtainedMarks2=+prompt('Enter obtained marks')
+document.write(ObtainedMarks2 +'<br>' +'\n')
+var totalmarks2=100
+
+
+var sub1=prompt('Enter subject name')
+document.write(sub1 +'\n')
+var ObtainedMarks3=+prompt('Enter obtained marks')
+document.write(ObtainedMarks3 +'<br>'+'\n')
+var totalmarks3=100
+
+var total=  ObtainedMarks1+ObtainedMarks2+ObtainedMarks3
+document.write('The obtaines marks is:'+total+"<br>" +'\n')
+var sum=totalmarks1+totalmarks2+totalmarks3
+document.write('The total marks is:'+sum+"<br>") +'\n'
+var result=total/sum*100
+document.write("The result is:"+result +'<br>' +'\n') 
+
+if (result>=90 && result<=100) {
+    document.write('The grade is A+ <br> well done')
+    
+}
+
+
+else if (result>=80 && result<90) {
+    document.write('The grade is B+\ <br> Good')
+    
+}
+
+else if (result>=70 && result<80) {
+    document.write('The grade is C+ <br> You need to improve')
+    
+}
+else{
+    document.write('The grade is F <br>You are fail')
+}
+*/
+
+/*
+Question2:
+var SecretNumber=7
+
+var UserInput=+prompt('Enter secret number')
+if (UserInput===7) {
+    document.write('Bingo! Correct answer')
+}
+
+     else if (UserInput===6){
+       document.write('Close enough to the correct answer')
+    }
+*/
+
+/*
+Question3:
+var Temperature=+prompt("Enter temperature")
+if(Temperature>40){
+document.write('It is too hot outside.')
+}
+else if(Temperature>30){
+document.write('The Weather today is Normal')
+}
+else if(Temperature>20){
+document.write('Today’s Weather is cool.')
+}
+else if(Temperature>10){
+document.write('OMG! Today’s weather is so Cool.')
+}
+*/
+/*
+Question4:
+var FirstNumber = +prompt("Enter a number");
+var SecondNumber = +prompt("Enter another number");
+var Operation1 = prompt("Enter any operation");
+var Operation2 = prompt("Enter any operation");
+var Operation3 = prompt("Enter any operation");
+var Operation4 = prompt("Enter any operation");
+var Operation5 = prompt("Enter any operation");
+var Addition = FirstNumber + SecondNumber;
+var Subtarction = FirstNumber - SecondNumber;
+var Multiplication = FirstNumber * SecondNumber;
+var Division = FirstNumber / SecondNumber;
+var Modulus = FirstNumber % SecondNumber;
+if (
+  Operation1 === "+" &&
+  Operation2 === "-" &&
+  Operation3 === "*" &&
+  Operation4 === "/"&&
+  Operation5==='%'
+) {
+  document.write(
+    "The sum is:\n" +
+      Addition +
+      "<br>" +
+      "The difference is:\n" +
+      Subtarction +
+      "<br>" +
+      "The multiplication is\n" +
+      Multiplication +
+      '<br>'+
+      "The division is:\n" +
+      Division+'<br>'+
+      'The modulus is \n":'+
+Modulus
+  );
+}
+*/
+
